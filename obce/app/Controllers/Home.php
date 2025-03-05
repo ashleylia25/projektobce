489104b670eb4e90b607b4b15de1d3d5a307c4d5
ashleylia25 <meldylia25@gmail.com>
2025-03-05 14:34:17 +0100

    `Refactor Home controller and add logging functionality`

--- obce/app/Controllers/Home.php
+++ obce/app/Controllers/Home.php
@@ -6,7 +6,46 @@ use App\Models\OkresModel;
 use App\Models\ObecModel;
 
 class Home extends BaseController
+{use CodeIgniter\I18n\Time;
+use CodeIgniter\Log\Logger;
+use CodeIgniter\Log\Handlers\FileHandler;
+
+class Home extends BaseController
 {
+    private $logger;
+
+    public function __construct()
+    {
+        $this->logger = new Logger(new FileHandler());
+    }
+
+    public function index(): string
+    {
+        $this->logger->info('Home index page loaded');
+        return view('welcome_message');
+    }
+
+    public function okresy()
+    {
+        $this->logger->info('Okresy page loaded');
+        $okresModel = new OkresModel();
+        $data['okresy'] = $okresModel->findAll();
+        return view('districts', $data);
+    }
+
+    public function obce($okresId)
+    {
+        $this->logger->info('Obce page loaded for okresId ' . $okresId);
+        $obecModel = new ObecModel();
+        $perPage = 20;
+        $currentPage = $this->request->getVar('strana') ? $this->request->getVar('strana') : 1;
+
+        $data['obce'] = $obecModel->getObceByOkres($okresId, $perPage, $currentPage);
+        $data['pager'] = $obecModel->pager;
+        return view('obce', $data);
+    }
+}
+}
     public function index(): string
     {
         return view('welcome_message');
@@ -29,4 +68,4 @@ class Home extends BaseController
         $data['pager'] = $obecModel->pager;
         return view('obce', $data);
     }
-}
+
